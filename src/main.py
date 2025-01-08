065021345785410c13f9f753e3cb2d88d5c05f42
babloo <bhuvaneshaashi@gmail.com>
2025-01-08 11:25:41 -0500

    main.py and requirements.txt updated

--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
 from deepgram import DeepgramClient, DeepgramClientOptions, LiveTranscriptionEvents, LiveOptions
-import httpx
+import pyaudio
 import threading
 
 # The API key you created in step 1
-DEEPGRAM_API_KEY = '916d021522ce7663133b4387d7b6cd37f367fb67'
-
-# URL for the real-time streaming audio you would like to transcribe
-URL = 'http://stream.live.vc.bbcmedia.co.uk/bbc_world_service'
+DEEPGRAM_API_KEY = '5068995718d38dbe230909ce1a989e3365dd8804'
 
 def main():
     try:
@@ -31,26 +28,40 @@ def main():
         lock_exit = threading.Lock()
         exit = False
 
-        # Listen for the connection to open and send streaming audio from the URL to Deepgram
+        # Listen for the connection to open and send streaming audio from the microphone to Deepgram
         def myThread():
-            with httpx.stream('GET', URL) as r:
-                for data in r.iter_bytes():
+            p = pyaudio.PyAudio()
+            stream = p.open(
+                format=pyaudio.paInt16,  # 16-bit audio format
+                channels=1,  # Mono audio
+                rate=16000,  # Sampling rate
+                input=True,  # Specify that this is an input stream
+                frames_per_buffer=1024  # Size of each audio chunk
+            )
+
+            try:
+                while True:
+                    audio_data = stream.read(1024, exception_on_overflow=False)
                     lock_exit.acquire()
                     if exit:
                         break
                     lock_exit.release()
 
-                    dg_connection.send(data)
+                    dg_connection.send(audio_data)
+            finally:
+                stream.stop_stream()
+                stream.close()
+                p.terminate()
 
-        myHttp = threading.Thread(target=myThread)
-        myHttp.start()
+        myMic = threading.Thread(target=myThread)
+        myMic.start()
 
         input('Press Enter to stop transcription...\n')
         lock_exit.acquire()
         exit = True
         lock_exit.release()
 
-        myHttp.join()
+        myMic.join()
 
         # Indicate that we've finished by sending the close stream message
         dg_connection.finish()
